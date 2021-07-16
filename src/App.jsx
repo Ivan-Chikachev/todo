64058f73d6413dd19da80b327c625bae7414c22c
Чикачев Иван <qschool_chikachev.i@qsoft.ru>
2021-07-16 14:46:40 +0300

    add timer for task

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import NewTaskForm from './components/NewTaskForm';
 import Footer from './components/Footer';
@@ -6,109 +6,119 @@ import TaskList from './components/TaskList';
 
 export default class App extends Component {
     state = {
-      todoData: [
-        {
-          id: 1,
-          name: 'Completed task',
-          done: true,
-          status: 'completed',
-        },
-        {
-          id: 2,
-          name: 'Editing task',
-          done: true,
-          status: 'editing',
-        },
-        {
-          id: 3,
-          name: 'Active task',
-          done: true,
-          status: '',
-        },
-      ],
-      filterTasks: 'All',
+        todoData: [
+            {
+                id: 1,
+                name: 'Completed task',
+                done: true,
+                status: 'completed',
+                min: 10,
+                sec: 10,
+            },
+            {
+                id: 2,
+                name: 'Editing task',
+                done: true,
+                status: 'editing',
+                min: 11,
+                sec: 0,
+            },
+            {
+                id: 3,
+                name: 'Active task',
+                done: true,
+                status: '',
+                min: 5,
+                sec: 0,
+            },
+        ],
+        filterTasks: 'All',
     };
 
-    createTask = (text) => ({
-      id: Date.now(),
-      name: text,
-      done: true,
-      status: '',
+    createTask = (text, min, sec) => ({
+        id: Date.now(),
+        name: text,
+        done: true,
+        status: '',
+        min,
+        sec,
     });
 
     onTaskClick = (id) => {
-      this.setState(({ todoData }) => ({
-        todoData: todoData.map((task) => {
-          if (task.id === id) {
-            return {
-              ...task,
-              done: !task.done,
-            };
-          }
-          return task;
-        }),
-      }));
+        this.setState(({todoData}) => ({
+            todoData: todoData.map((task) => {
+                if (task.id === id) {
+                    return {
+                        ...task,
+                        done: !task.done,
+                    };
+                }
+                return task;
+            }),
+        }));
     };
 
     deleteTask = (id) => {
-      this.setState(({ todoData }) => ({
-        todoData: todoData.filter((i) => i.id !== id),
-      }));
+        this.setState(({todoData}) => ({
+            todoData: todoData.filter((i) => i.id !== id),
+        }));
     };
 
-    addTask = (text) => {
-      this.setState(({ todoData }) => {
-        text = text.trim();
-        if (text === '') return;
-        const item = this.createTask(text);
-        return {
-          todoData: [...todoData, item],
-        };
-      });
+    addTask = (text, min, sec) => {
+        this.setState(({todoData}) => {
+            text = text.trim();
+            if (text === '') return;
+            if (min === '') min = 10;
+            if (sec === '') sec = 0;
+            const item = this.createTask(text, min, sec);
+            return {
+                todoData: [...todoData, item],
+            };
+        });
     };
 
     clearCompletedTasks = () => {
-      this.setState(({ todoData }) => ({ todoData: todoData.filter((i) => i.done === true) }));
+        this.setState(({todoData}) => ({todoData: todoData.filter((i) => i.done === true)}));
     };
 
     setFilter = (name) => {
-      this.setState(() => ({ filterTasks: name }));
+        this.setState(() => ({filterTasks: name}));
     };
 
     render() {
-      const FILTER_MAP = {
-        All: () => true,
-        Active: (task) => task.done,
-        Completed: (task) => !task.done,
-      };
+        const FILTER_MAP = {
+            All: () => true,
+            Active: (task) => task.done,
+            Completed: (task) => !task.done,
+        };
 
-      const FILTER_NAMES = Object.keys(FILTER_MAP);
+        const FILTER_NAMES = Object.keys(FILTER_MAP);
 
-      const countItems = this.state.todoData.filter((i) => i.done === false).length;
-      return (
-        <section className="todoapp">
-          <header className="header">
-            <h1>todos</h1>
-            <NewTaskForm addTask={this.addTask} />
-          </header>
-          <section className="main">
-            <TaskList
-              addTask={this.addTask}
-              deleteTask={this.deleteTask}
-              onTaskClick={this.onTaskClick}
-              todoData={this.state.todoData}
-              filterMap={FILTER_MAP}
-              filterName={this.state.filterTasks}
-            />
-            <Footer
-              setFilter={this.setFilter}
-              isPressed={this.state.filterTasks}
-              clearCompletedTasks={this.clearCompletedTasks}
-              countItems={countItems}
-              filterList={FILTER_NAMES}
-            />
-          </section>
-        </section>
-      );
+        const countItems = this.state.todoData.filter((i) => i.done === false).length;
+        return (
+            <section className="todoapp">
+                <header className="header">
+                    <h1>todos</h1>
+                    <NewTaskForm addTask={this.addTask}/>
+                </header>
+                <section className="main">
+                    <TaskList
+                        addTask={this.addTask}
+                        deleteTask={this.deleteTask}
+                        onTaskClick={this.onTaskClick}
+                        todoData={this.state.todoData}
+                        filterMap={FILTER_MAP}
+                        filterName={this.state.filterTasks}
+                    />
+                    <Footer
+                        setFilter={this.setFilter}
+                        isPressed={this.state.filterTasks}
+                        clearCompletedTasks={this.clearCompletedTasks}
+                        countItems={countItems}
+                        filterList={FILTER_NAMES}
+                    />
+                </section>
+            </section>
+        );
     }
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,39 +3,41 @@ import PropTypes from 'prop-types';
 import TasksFilter from './TasksFilter';
 
 const Footer = ({
-  countItems, clearCompletedTasks, filterList, isPressed, setFilter,
-}) => (
-  <footer className="footer">
+                    countItems, clearCompletedTasks, filterList, isPressed, setFilter,
+                }) => (
+    <footer className="footer">
     <span className="todo-count">
       {countItems}
-      {' '}
-      items left
+        {' '}
+        items left
     </span>
-    <TasksFilter
-      filterList={filterList}
-      setFilter={setFilter}
-      isPressed={isPressed}
-    />
-    <button
-      onClick={clearCompletedTasks}
-      className="clear-completed"
-    >
-      Clear completed
-    </button>
-  </footer>
+        <TasksFilter
+            filterList={filterList}
+            setFilter={setFilter}
+            isPressed={isPressed}
+        />
+        <button
+            onClick={clearCompletedTasks}
+            className="clear-completed"
+        >
+            Clear completed
+        </button>
+    </footer>
 );
 
 Footer.defaultProps = {
-  clearCompletedTasks: () => {},
-  setFilter: () => {},
+    clearCompletedTasks: () => {
+    },
+    setFilter: () => {
+    },
 };
 
 Footer.propTypes = {
-  countItems: PropTypes.number.isRequired,
-  clearCompletedTasks: PropTypes.func,
-  filterList: PropTypes.arrayOf(PropTypes.string).isRequired,
-  isPressed: PropTypes.string.isRequired,
-  setFilter: PropTypes.func,
+    countItems: PropTypes.number.isRequired,
+    clearCompletedTasks: PropTypes.func,
+    filterList: PropTypes.arrayOf(PropTypes.string).isRequired,
+    isPressed: PropTypes.string.isRequired,
+    setFilter: PropTypes.func,
 };
 
 export default Footer;
